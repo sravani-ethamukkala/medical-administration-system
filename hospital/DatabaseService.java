64a107f135eb7916b8a68c797e2beadfb03a8d4a
sravani-ethamukkala <sravaniethamukkala@gmail.com>
2024-11-28 12:31:20 +0530

    Add files via upload

---
+++ hospital/DatabaseService.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseService {
+	public static Connection conn;
+
+	private static Connection createConn() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital", "root", "Sravani@73");
+
+		System.out.println("Database connection created successfully.");
+
+		return conn;
+	}
+
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		if (conn == null) {
+			return createConn();
+		}
+
+		return conn;
+	}
+}
